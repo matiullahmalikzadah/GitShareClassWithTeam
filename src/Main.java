badfe068b47fa6984b0b924772baf5bdccab67c6
matiullahmalikzadah <matiullah198611@gmail.com>
2024-07-29 12:06:05 -0700

    this commit has the basic stucture of the Project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("hhhhhhhhhhhhh");
+        System.out.println("Hmghjdthjth");
+        System.out.println("Hrtert45gg");
+        System.out.println("q3454gttrvb");
+
+
+    }
+}
